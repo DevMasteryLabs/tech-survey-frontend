86654e8efeea065c14927f745bbdab8a74dc8cb6
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-29 00:04:02 +0100

    Add a sticky footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import FileUpload from './components/FileUpload';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
 
 const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className="bi bi-cloud-arrow-up-fill"></i> Tech Survey
-    </h4>
-    <FileUpload />
-  </div>
+  <Wrapper>
+    <div className='container mt-4'>
+      <h4 className='display-4 text-center mb-4'>
+        <i className="bi bi-cloud-arrow-up-fill"></i> Tech Survey
+      </h4>
+      <FileUpload />
+    </div>
+    <Footer />
+  </Wrapper>
 );
 
 export default App;
